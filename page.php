e4714613c535b540eeef5417c146b3856abae5bf
Alejandro Reyna <AleejandroReyna@gmail.com>
2019-11-28 16:15:37 -0600

    Basic styles for posts, page and footer.

---
+++ page.php
@@ -0,0 +1,13 @@
+<?php get_header(); ?>
+	<section class="page-content">
+		<div class="container">
+			<?php
+			if(have_posts()) {
+				while (have_posts()) {
+					the_post();
+					the_content();
+				}
+			}?>
+		</div>
+	</section>
+<?php get_footer(); ?>
