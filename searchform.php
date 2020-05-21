54e1b253495e5e8f82a8e7e6eaae118810f1c827
Alejandro Reyna <AleejandroReyna@gmail.com>
2020-05-21 10:07:37 -0600

    gitflow-hotfix-stash: added-title-faqpage
    Changed form label, added title in page.

--- searchform.php
+++ searchform.php
@@ -1,5 +1,5 @@
 <form role="search" method="get" id="searchform" class="header-search-form" action="<?php echo home_url( '/' ); ?>">
     <button type="submit" class="button"><i class="fas fa-search no-margin"></i></button>
 	<input type="hidden" name="post_type" value="faq" />
-	<input class="input" value="" name="s" id="s" type="text" placeholder="Buscar">
+	<input class="input" value="" name="s" id="s" type="text" placeholder="">
 </form>
